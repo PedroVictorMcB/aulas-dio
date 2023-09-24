01e80d4692dda65a3a291769e2cb05a943d88bbd
PedroVíctorMCB <pedrovictormcb80@gmail.com>
2023-09-24 18:15:25 -0300

    Learned about arrow fuction, method get request, promises on fetch. Made a method request get on main.js, added content to dicas.txt, minor changes into index.html

--- JS/Pokedex/assests/js/main.js
+++ JS/Pokedex/assests/js/main.js
@@ -1,1 +1,28 @@
-console.log("Sucess!");
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json())
+    // exemplo de arrow function na ln 7
+    .then(function(jsonBody) {
+        console.log(jsonBody);
+    })
+    // then = caso tenha sucesso na resposta da requisição
+    .catch(function(error) {
+        console.log(error);
+    })
+    // catch = caso tenha algum erro na resposta da requisição
+    
+    // .finally(function() {
+    //     console.log("Requisição Concluída!")
+    // })
+    
+    // finally = responde independentemente se tem sucesso ou fracasso na resposta da requisição
+
+    // Esta estrutura acima é um modelo de uma interface de comunicação de uma PROMISE. Esta estrutura se assemelha com a estrutura de tratamento de erro Try-Catch.
+    // No try-catch o try equivale ao .then, o catch ao .catch e o finally ao .finally
+
+const x = 10 + 10;
+console.log(x);
