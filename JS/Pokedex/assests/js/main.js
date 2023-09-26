d8b53e294e77ad8aac471b5b1a6dace822b7c491
PedroVíctorMCB <pedrovictormcb80@gmail.com>
2023-09-26 16:16:11 -0300

    Used DOM on poke-api.js and main.js, in order to turn dynamicity to <li>

--- JS/Pokedex/assests/js/main.js
+++ JS/Pokedex/assests/js/main.js
@@ -1,16 +1,19 @@
 
+function convertPokemonTypesToLI (pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLI (pokemon) {
     return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name"> ${pokemon.name}</span>
         
         <div class="detail">
             <ol class="types">
-                <li class="type">Grass</li>
-                <li class="type">Poison</li>
+                ${convertPokemonTypesToLI(pokemon.types).join('')}
             </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name} image">
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name} image">
         </div>
     </li>
     `
@@ -22,13 +25,22 @@ const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
     
-    const newList = pokemons.map((pokemon) => {
-        return convertPokemonToLI(pokemon)
-    })
+    const newHTML = pokemons.map(convertPokemonToLI).join('');
+    pokemonList.innerHTML = newHTML
 
-    const newHTML = newList.join('');
 
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLI).join('');
+    // const newList = pokemons.map((pokemon) => {
+    //     return convertPokemonToLI(pokemon)
+    // })
+
+
+
+
+
+
+    // const newHTML = newList.join('');
+
+    // pokemonList.innerHTML += pokemons.map(convertPokemonToLI).join('');
 
 
     // const listItems = [];
--- JS/Pokedex/assests/js/poke-api.js
+++ JS/Pokedex/assests/js/poke-api.js
@@ -1,12 +1,19 @@
 
 const pokeApi = {};
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 // att definidos por default
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
         // exemplo de arrow function na ln 7. Mais utilizado no mercado.
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonsDetails) => pokemonsDetails)
         .catch((error) => console.error(error))
 }
