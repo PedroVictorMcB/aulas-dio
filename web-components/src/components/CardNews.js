c3580a804d60710dfc5f116184ab6fbe1fc92c30
PedroVictorMcB <pedrovictormcb80@gmail.com>
2023-10-03 16:30:15 -0300

    Made the base of a personalized component. Still working on

--- web-components/src/components/CardNews.js
+++ web-components/src/components/CardNews.js
@@ -2,7 +2,15 @@ class CardNews extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello World</h1>";
+        shadow.innerHTML = "<h1>Hello Peter/h1>";
+    }
+
+    build() {
+
+    }
+
+    style() {
+        
     }
 }
 
