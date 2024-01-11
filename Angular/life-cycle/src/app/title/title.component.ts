842b43eddb2f1132ffdd8ade19e1f19e56e6dc21
PedroVictorMcB <pedrovictormcb80@gmail.com>
2024-01-11 19:25:45 -0300

    add content to dicas.txt. Learned about OnInit and OnChanges' hooks

--- Angular/life-cycle/src/app/title/title.component.ts
+++ Angular/life-cycle/src/app/title/title.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-  name: string = "Walter Sullivan"
+  @Input() name: string = ""
 
-  constructor () {}
+  constructor () {
+      console.log("Constructor");
+    }
+
+  // Onchanges é um evento disparado sempre que o valor de uma propriedade é alterado
+  ngOnChanges(): void {
+    console.log("OnChange");
+  }
 
   //OnItnit é um evento que será disparado sempre que o componente for iniciado
   ngOnInit(): void {
-    this.name = `Ola ${this.name}`;
+    console.log("OnInit");
   }
 }
